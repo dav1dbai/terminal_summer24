fe64a21fd723b670d1a593f04bf22037f59da63e
David Bai <davidbai2020@gmail.com>
2024-08-18 22:04:37 -0700

    opponent logging

---
+++ db_opponents/algo_strategy.py
@@ -0,0 +1,234 @@
+import gamelib
+import random
+import math
+import warnings
+from sys import maxsize
+import json
+
+
+"""
+Most of the algo code you write will be in this file unless you create new
+modules yourself. Start by modifying the 'on_turn' function.
+
+Advanced strategy tips: 
+
+  - You can analyze action frames by modifying on_action_frame function
+
+  - The GameState.map object can be manually manipulated to create hypothetical 
+  board states. Though, we recommended making a copy of the map to preserve 
+  the actual current map state.
+"""
+
+class AlgoStrategy(gamelib.AlgoCore):
+    def __init__(self):
+        super().__init__()
+        seed = random.randrange(maxsize)
+        random.seed(seed)
+        gamelib.debug_write('Random seed: {}'.format(seed))
+
+    def on_game_start(self, config):
+        """ 
+        Read in config and perform any initial setup here 
+        """
+        gamelib.debug_write('Configuring your custom algo strategy...')
+        self.config = config
+        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP
+        WALL = config["unitInformation"][0]["shorthand"]
+        SUPPORT = config["unitInformation"][1]["shorthand"]
+        TURRET = config["unitInformation"][2]["shorthand"]
+        SCOUT = config["unitInformation"][3]["shorthand"]
+        DEMOLISHER = config["unitInformation"][4]["shorthand"]
+        INTERCEPTOR = config["unitInformation"][5]["shorthand"]
+        MP = 1
+        SP = 0
+        # This is a good place to do initial setup
+        self.scored_on_locations = []
+        self.opponent_spawns = []
+        self.predicted_opponent_spawns = []
+        self.printed = True
+
+    def on_turn(self, turn_state):
+        """
+        This function is called every turn with the game state wrapper as
+        an argument. The wrapper stores the state of the arena and has methods
+        for querying its state, allocating your current resources as planned
+        unit deployments, and transmitting your intended deployments to the
+        game engine.
+        """
+        game_state = gamelib.GameState(self.config, turn_state)
+        gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
+        game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
+
+        self.starter_strategy(game_state)
+
+        game_state.submit_turn()
+
+
+    """
+    NOTE: All the methods after this point are part of the sample starter-algo
+    strategy and can safely be replaced for your custom algo.
+    """
+
+    def starter_strategy(self, game_state):
+        """
+        For defense we will use a spread out layout and some interceptors early on.
+        We will place turrets near locations the opponent managed to score on.
+        For offense we will use long range demolishers if they place stationary units near the enemy's front.
+        If there are no stationary units to attack in the front, we will send Scouts to try and score quickly.
+        """
+        # calculate opponent theoretical spawns, for last game (before deploy phase of turn that this is for)
+        opponent_all_edges = game_state.game_map.get_edge_locations(0) + game_state.game_map.get_edge_locations(1)
+        #gamelib.debug_write(opponent_all_edges)
+        opponent_potential_spawns = self.filter_blocked_locations(opponent_all_edges, game_state)
+        # get n best possible spawns
+        opponent_best_spawns = []
+        for i in range(2):
+            location = self.least_damage_spawn_location_enemy(game_state,opponent_potential_spawns)
+            opponent_best_spawns.append(location)
+            opponent_potential_spawns.remove(location)
+        self.predicted_opponent_spawns.append(opponent_best_spawns)
+
+       #once per turn
+        gamelib.debug_write("predicted opponent spawns")
+        gamelib.debug_write(self.predicted_opponent_spawns)
+        gamelib.debug_write("actual opponent spawns")
+        gamelib.debug_write(self.opponent_spawns)
+
+        # First, place/restore basic defenses
+        self.build_defences(game_state)
+        # Now build reactive defenses based on where the enemy scored
+        self.build_reactive_defense(game_state)
+        
+        if game_state.turn_number % 2 == 0:
+            game_state.attempt_spawn(SCOUT, [13, 0], 1000)
+            return
+
+        #build support cannon
+        self.build_support_cannon(game_state)
+
+    def build_defences(self, game_state):
+        """
+        Build basic defenses using hardcoded locations.
+        Remember to defend corners and avoid placing units in the front where enemy demolishers can attack them.
+        """
+        # Useful tool for setting up your base locations: https://www.kevinbai.design/terminal-map-maker
+        # More community tools available at: https://terminal.c1games.com/rules#Download
+
+        # Place turrets that attack enemy units
+        turret_locations = [[4, 12], [9, 12], [17, 12], [23, 12]]
+        # attempt_spawn will try to spawn units if we have resources, and will check if a blocking unit is already there
+        game_state.attempt_spawn(TURRET, turret_locations)
+        
+        upgrade_turret_locations = [[4, 12], [17, 12]]
+        game_state.attempt_upgrade(upgrade_turret_locations)
+
+    def build_support_cannon(self, game_state):
+        '''
+        Build support diagonal to buff mobile units
+        '''
+        for x in range(13,22):
+            y = x - 12
+            game_state.attempt_spawn(SUPPORT, [x, y])
+
+    def build_reactive_defense(self, game_state):
+        """
+        This function builds reactive defenses based on where the enemy scored on us from.
+        We can track where the opponent scored by looking at events in action frames 
+        as shown in the on_action_frame function
+        """
+        for location in self.scored_on_locations:
+            # Build turret one space above so that it doesn't block our own edge spawn locations
+            build_location = [location[0], location[1]+1]
+            game_state.attempt_spawn(TURRET, build_location)
+
+
+    def least_damage_spawn_location(self, game_state, location_options):
+        """
+        This function will help us guess which location is the safest to spawn moving units from.
+        It gets the path the unit will take then checks locations on that path to 
+        estimate the path's damage risk.
+        """
+        damages = []
+        # Get the damage estimate each path will take
+        for location in location_options:
+            path = game_state.find_path_to_edge(location)
+            damage = 0
+            for path_location in path:
+                # Get number of enemy turrets that can attack each location and multiply by turret damage
+                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(TURRET, game_state.config).damage_i
+            damages.append(damage)
+        
+        # Now just return the location that takes the least damage
+        return location_options[damages.index(min(damages))]
+    
+    def least_damage_spawn_location_enemy(self,game_state,location_options):
+        damages = []
+        for location in location_options:
+            path = game_state.find_path_to_edge(location)
+            damage = 0
+            for path_location in path:
+                # Get number of ally turrets that can attack each location and multiply by turret damage
+                damage += len(game_state.get_attackers(path_location, 1)) * gamelib.GameUnit(TURRET, game_state.config).damage_i
+            damages.append(damage)
+        
+        # Now just return the location that takes the least damage
+        return location_options[damages.index(min(damages))]
+
+    def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
+        total_units = 0
+        for location in game_state.game_map:
+            if game_state.contains_stationary_unit(location):
+                for unit in game_state.game_map[location]:
+                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
+                        total_units += 1
+        return total_units
+        
+    def filter_blocked_locations(self, locations, game_state):
+        filtered = []
+        for location in locations:
+            if not game_state.contains_stationary_unit(location):
+                filtered.append(location)
+        return filtered
+
+    def on_action_frame(self, turn_string):
+        """
+        This is the action frame of the game. This function could be called 
+        hundreds of times per turn and could slow the algo down so avoid putting slow code here.
+        Processing the action frames is complicated so we only suggest it if you have time and experience.
+        Full doc on format of a game frame at in json-docs.html in the root of the Starterkit.
+        """
+        # Let's record at what position we get scored on
+        state = json.loads(turn_string)
+        events = state["events"]
+        turninfo = state["turnInfo"]
+        # check deploy phase to identify spawn locations
+        if self.printed:
+            opponent_stats = state["p2Units"]
+            opponent_units = {
+                "scouts": opponent_stats[3],
+                "demolishers" : opponent_stats[4],
+                "interceptors": opponent_stats[5],
+            }
+            #gamelib.debug_write("turn {}".format(turnInfo[1]))
+            #gamelib.debug_write("actual opponent spawns")
+            self.opponent_spawns.append(opponent_units)
+            self.printed = False
+        
+        self.printed = True
+        
+        breaches = events["breach"]
+        for breach in breaches:
+            location = breach[0]
+            unit_owner_self = True if breach[4] == 1 else False
+            # When parsing the frame data directly, 
+            # 1 is integer for yourself, 2 is opponent (StarterKit code uses 0, 1 as player_index instead)
+            if not unit_owner_self:
+                #gamelib.debug_write("Got scored on at: {}".format(location))
+                self.scored_on_locations.append(location)
+                #gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
+            
+
+
+if __name__ == "__main__":
+    algo = AlgoStrategy()
+    algo.start()
